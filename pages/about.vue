3465be17e5019abcd58fd0fbce7940e0b81eb716
abduallahnoorsayket <abdullahnoorsayket@gmail.com>
2022-09-29 14:41:21 +0600

    first app

---
+++ pages/about.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>this is the about page.</h1>
+    <hr />
+    <NuxtLink to="/">Home page</NuxtLink>
+    <hr />
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <hr />
+    <nuxt-mountains />
+    <hr />
+    <mountain-list />
+    <hr />
+    <NuxtLink to="/about">About page</NuxtLink>
+    <hr />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "IndexPage",
+};
+</script>
